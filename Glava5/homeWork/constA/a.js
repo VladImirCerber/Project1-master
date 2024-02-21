7aba041fb96e975e65038604679e2ec804f57f84
Vladimir <e[ample@gmail.com>
2024-02-21 15:43:52 +0300

    21.02.24

---
+++ Glava5/homeWork/constA/a.js
@@ -0,0 +1,16 @@
+const a = [];
+
+a['key1'] = 1;
+a['key2'] = 1;
+a['key3'] = 1;
+
+a['3'] = 1;
+a[3] = 1;
+
+console.log(a.length);
+
+
+let num = 0;
+const list = ['hey1', 'hey2', 'hey3', 'hey4'];
+
+console.log(list[num + 1]);
