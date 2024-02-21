9a557aa377c594c4f566955745084bb32c53096a
Vladimir <e[ample@gmail.com>
2024-02-21 17:04:48 +0300

    21.02.24

--- Glava5/homeWork/constA/a.js
+++ Glava5/homeWork/constA/a.js
@@ -14,3 +14,34 @@ let num = 0;
 const list = ['hey1', 'hey2', 'hey3', 'hey4'];
 
 console.log(list[num + 1]);
+
+
+let resulst =  5>6 ? console.log('true') : console.log('false')
+//Колбэки 
+
+const first = (callback) => {
+    //Как будто бы запрос к API
+    setTimeout(() => {
+        console.log(1);
+        callback();
+    }, 500);
+    
+};
+
+const second = () => {
+    console.log(2);
+};
+
+first(second);
+
+
+
+
+const doHomework = (subject, callback) => {
+    alert(`Starting my ${subject} homework`);
+    callback();
+};
+
+doHomework('math', () => {
+    alert('Finished my homework');
+});
