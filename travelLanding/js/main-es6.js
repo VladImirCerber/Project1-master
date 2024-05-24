1d6748274e1254f99bf79f6c46cc10ca9a9cf0ae
Vladimir <e[ample@gmail.com>
2024-05-24 16:49:26 +0300

    24.05.24struct

--- travelLanding/js/main-es6.js
+++ travelLanding/js/main-es6.js
@@ -32,6 +32,17 @@
     }
 }());
 
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  /* Установите ширину боковой панели на 0, а левое поле содержимого страницы - на 0 */
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
+
 // Scroll to anchors
 (function () {
 
